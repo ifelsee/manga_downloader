8975a7b194c4410dae183e1c7466645f999819dd
ifelsee <None>
2021-04-21 02:16:36 +0300

    İndirme Sorunları çözüldü

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import wget
 from bs4 import BeautifulSoup
 import time
 import os 
+import re
 
-
-os.chdir("One Piece")
+os.chdir("Attack On Titan")
 
 def download_Chapters(r):
     soup  = BeautifulSoup(r.content, 'html.parser')
@@ -14,32 +14,52 @@ def download_Chapters(r):
         if  link.get("data-src")!= None:
             full_link = str("https://"+link.get("data-src").split("//")[-1])
             full_link = full_link[:-1]
-            print(full_link[-3:])
-            if full_link[-3:] == "jpg":
+            chapter_num = full_link[-6:-4]
+            have_chapther = False
+            for dirs in os.listdir():
+                if dirs[-3:] == "tmp":
+                    os.remove(dirs)
+                    print("tmp Silindi! Sağlam Dosya İndiriliyor")
+                elif  dirs[:-4] == str(chapter_num) and dirs:
+                    have_chapther = True
+                    print("dirs num = ",dirs, "  chapter num = ", chapter_num )
+            if full_link[-3:] == "jpg" and have_chapther == False:
+                print("++++++++++++",chapter_num,"-----------------")
+                print(" --  ",full_link[-3:])
                 wget.download(full_link)            
 
 
 
-def episode_finder_func():
+def episode_finder_func(wtart_episode):
      
     episode_finder = True
     first_episode_finded = False
-    episode = 700
+    episode = 1 
     episode_zero = 0
 
     while episode_finder:
         # !flag kabul etmedi
-        if episode >= 1000: 
-            r = requests.get("https://mangadenizi.com/manga/one-piece/{episode}".format(episode = episode))
+        if episode >= 1000 or episode >=100:
+            r = requests.get("https://mangadenizi.com/manga/attack-on-titan/{episode}".format(episode = episode))
         else:       
-            r = requests.get("https://mangadenizi.com/manga/one-piece/{episode_zero}{episode}".format(episode_zero=episode_zero,episode = episode))
+            r = requests.get("https://mangadenizi.com/manga/attack-on-titan/{episode_zero}{episode}".format(episode_zero=episode_zero,episode = episode))
         if r.status_code == 200:
             print("bölüm bulundu ", episode)
             first_episode_finded = True
-            os.mkdir(str(episode))
-            os.chdir(str(episode)) 
-            download_Chapters(r)
-            os.chdir("../")
+            have_episode = False
+            for dirs in os.listdir():
+                if dirs == str(episode):
+                    have_episode = False
+
+            if have_episode == False:
+                try:
+                    os.mkdir(str(episode))
+                except:
+                    print("Dosya yaratılamadı")    
+                os.chdir(str(episode)) 
+                download_Chapters(r)
+                os.chdir("../")
+
             episode = episode +1
         elif r.status_code == 500:
             if first_episode_finded == True:
@@ -52,9 +72,8 @@ def episode_finder_func():
 
 
 def main():
-    episode_finder_func()
+    episode_finder_func(1)
 
 
-main()
 
 
